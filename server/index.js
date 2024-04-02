c76cafec9cfee6d69e518b8d7507fc61f99720e2
vikas <vikaskumar638727@gmail.com>
2024-04-02 18:35:30 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin:["https://facial-login-project.vercel.app"],
+  origin:["https://facial-login-project.vercel.app/"],
   credentials:true
 }));
 app.use(express.json());
